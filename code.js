ad08002895dcf8808883934431bd36b82bc2eb39
YY31-z <yuyanzang@gmail.com>
2025-04-01 11:42:18 +0200

    Update code.js

--- code.js
+++ code.js
@@ -1,5 +1,5 @@
 export const configurazione = {
-  testo: "ZY",
+  testo: "yy",
   dimensione: 0.8,
   interlinea: 0.7,
   allineamento: "centro",
@@ -36,35 +36,15 @@ export function disegnaPunto({
   beta = 0,
   gamma = 0,
 }) {
-  push();
-  translate(x, y);
-
-  noFill();
-  stroke(0);
-
-  // Use orientation data to influence color
-  // Map alpha (z-rotation) to hue (0-360)
-  const hue = map(alpha, 0, 360, 0, 360);
-
-  // Map beta (front-to-back tilt) to saturation (50-100)
-  const saturation = map(abs(beta), 0, 90, 50, 100);
-
-  // Map gamma (left-to-right tilt) to brightness (50-100)
-  const brightness = map(abs(gamma), 0, 90, 50, 100);
-
-  colorMode(HSB, 360, 100, 100);
-  fill(hue, saturation, brightness);
-  noStroke();
-
+  if (indice % 2 == 0) {
+    noStroke();
+    fill("white");
+  } else {
+    fill("pink");
+  }
   rectMode(CENTER);
-  rotate(frameCount + indice);
-
-  // Add slight variation based on device tilt
-  scale(1 + volume * 10 + (abs(gamma) / 90) * 0.5);
-  rect(0, 0, unita / 2);
-  pop();
+  rect(x, y, mouseX, mouseY);
 }
-
 //
 
 export function caricamentoRisorse() {}
@@ -81,7 +61,7 @@ export function impostazioni() {
 export function sotto(disegnaTesto) {
   background(255);
 
-  fill("deeppink");
+  fill("yellow");
   disegnaTesto();
 }
 
